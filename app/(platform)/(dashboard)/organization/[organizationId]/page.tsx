c73d307a17f306326b34ee3f2ae78fb06bfa4d58
Surya Adi Wiguna <142723064+suryaadiwiguna@users.noreply.github.com>
2024-10-27 15:39:19 +0700

    initial db connection

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,30 @@
+import { db } from "@/lib/db"
+
 export default function OrganizationIdPage() {
+    async function createBoard(formData: FormData) {
+        "use server"
+
+        const title = formData.get("title") as string
+
+        await db.board.create({
+            data: {
+                title
+            }
+        })
+
+    }
 
     return (
         <div>
-            Organization Page!
+            <form action={createBoard}>
+                <input
+                    id="title"
+                    name="title"
+                    required
+                    placeholder="Enter a board title"
+                    className="border-gray-400 border rounded-sm p-1 px-2"
+                />
+            </form>
         </div>
     )
 }
