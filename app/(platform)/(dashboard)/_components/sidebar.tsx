742915caa8470b79368407b0df626d084abd571e
Surya Adi Wiguna <suryaadiwiguna@gmail.com>
2024-12-01 08:11:05 +0700

    fixed any types on sidebar.tsx

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -6,7 +6,7 @@ import { useLocalStorage } from "usehooks-ts"
 import { useOrganization, useOrganizationList } from "@clerk/nextjs"
 
 import { Button } from "@/components/ui/button"
-import { Separator } from "@/components/ui/separator"
+// import { Separator } from "@/components/ui/separator"
 import { Skeleton } from "@/components/ui/skeleton"
 import { Accordion } from "@/components/ui/accordion"
 import NavItem, { Organization } from "./nav-items"
@@ -18,7 +18,7 @@ interface SidebarProps {
 export default function Sidebar({
     storageKey = "t-sidebar-state"
 }: SidebarProps) {
-    const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    const [expanded, setExpanded] = useLocalStorage<Record<string, boolean>>(
         storageKey,
         {}
     )
